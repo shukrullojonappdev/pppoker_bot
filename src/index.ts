55324c723d20cc1f88a84c20fb8ffe76b7151899
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-10-15 17:06:48 +0500

    bot

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { Bot } from "./bot";
-import { Server } from "./server";
 
 const bot = new Bot("5420356035:AAG_za1nsfUZvVDt-KBPJuMzLTqdOK2lOjw");
-const server = new Server();
 
 bot.start();
-server.start();
