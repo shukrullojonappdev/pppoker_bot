89fc226283e5d4173a699590211e7e95005d0f32
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-10-15 18:16:48 +0500

    bot

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import express from "express";
 import path from "path";
 
 const app = express();
+const router = express.Router();
+
 app.use(express.static(path.join(__dirname, "views")));
+app.use("/", router);
 
 const bot = new Bot(
   "5420356035:AAG_za1nsfUZvVDt-KBPJuMzLTqdOK2lOjw",
