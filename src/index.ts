cc983855df0551c022d974b8397ae8a898923993
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-10-18 10:09:32 +0500

    bot

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,11 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, "../views")));
 app.use(bodyParser());
 
-// const bot = new Bot(
-//   "5420356035:AAG_za1nsfUZvVDt-KBPJuMzLTqdOK2lOjw",
-//   "mongodb+srv://shukrullojondev:1234@pppokerbotdatabase.7zxbw4z.mongodb.net/?retryWrites=true&w=majority?directConnection=true"
-// );
-// bot.start();
+const bot = new Bot(
+  "5420356035:AAG_za1nsfUZvVDt-KBPJuMzLTqdOK2lOjw",
+  "mongodb+srv://shukrullojondev:1234@pppokerbotdatabase.7zxbw4z.mongodb.net/?retryWrites=true&w=majority?directConnection=true"
+);
+bot.start();
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/index.html"));
