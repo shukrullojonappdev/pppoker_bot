ebd48d4c2cf8a10cbf6e07bb324403145d1afb5f
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-10-18 09:36:50 +0500

    bot

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT || 5000, () => {
   console.log("server started.");
 });
