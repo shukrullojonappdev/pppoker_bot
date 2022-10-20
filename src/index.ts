143c2804aefead41c33119bcdf5f960ba07719f7
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-10-21 00:09:11 +0500

    bot

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, "../views")));
+app.use(express.static(path.join(__dirname, "../web-app/build")));
 app.use(bodyParser());
 
 app.use("/", router);
--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,9 @@ import path from "path";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  return await res.sendFile(path.join(__dirname, "../views/index.html"));
+  return await res.sendFile(
+    path.join(__dirname, "../web-app/build/index.html")
+  );
 });
 
 // router.get("/buyChips", async (req, res) => {
